093e4613bc26aefcd2741d9a34e6d58c718bfb93
Brandy Foster <brandy.foster@detroitlabs.com>
2014-10-09 15:31:49 -0400

    parsed JSON data.

---
+++ Sunshine/app/src/main/java/com/example/bfinerocks/sunshine/app/WeatherParser.java
@@ -0,0 +1,12 @@
+package com.example.bfinerocks.sunshine.app;
+
+import org.json.JSONObject;
+
+/**
+ * Created by BFineRocks on 10/9/14.
+ */
+public class WeatherParser {
+
+    JSONObject weatherString;
+
+}

56945324ae787a9cd3a4bb00de7e0d17e6c03a79
Brandy Foster <brandy.foster@detroitlabs.com>
2014-10-15 10:35:27 -0400

    created sql table constructor in data package

--- Sunshine/app/src/main/java/com/example/bfinerocks/sunshine/app/data/WeatherContract.java
+++ Sunshine/app/src/main/java/com/example/bfinerocks/sunshine/app/data/WeatherContract.java
@@ -7,6 +7,18 @@ import android.provider.BaseColumns;
  */
 public class WeatherContract {
 
+    public static final class LocationEntry implements BaseColumns{
+        /*
+        must contain a city name, contain the table name and name of each colmn
+         */
+
+        public static final String TABLE_NAME = "location";
+        public static final String COLUMN_CITY_NAME = 'city_name';
+        public static final String COLUMN_LOCATION_SETTING = "location_setting";
+        public static final String COLUMN_COORD_LAT = "coord_lat";
+        public static final String COLUMN_COORD_LONG = "coord_long";
+    }
+
   public static final class WeatherEntry implements BaseColumns{
       public static final String TABLE_NAME = "weather";
       public static final String COLUMN_LOC_KEY = "location_id";
@@ -18,6 +30,7 @@ public class WeatherContract {
       public static final String COLUMN_HUMIDITY = "humidity";
       public static final String COLUMN_PRESSURE = "pressure";
       public static final String COLUMN_WIND_SPEED = "wind";
+      public static final String COLUMN_DEGREES = "degrees";
 
   }
 
